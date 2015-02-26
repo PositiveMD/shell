e08b876b04852af0ffda0c3b619b8073151a4a6c
Anthony Dinh <adinh182@knights.ucf.edu>
2015-02-26 08:28:43 -0500

    write split line function

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_LOK_DELIM " \t\r\n\a"
 
 int main(int argc, char **argv)
 {
@@ -65,7 +67,36 @@ char *lsh_read_line(void)
             exit(EXIT_FAILURE);
         }
     }
+}
 
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE , position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
 
+    if (!tokens){
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_LOK_DELIM);
+    while (token != NULL){
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize){
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens){
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_LOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
 
